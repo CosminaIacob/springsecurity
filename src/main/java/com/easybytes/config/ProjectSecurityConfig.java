04774d85aef8375b9f907d2b3e56279cb232d0eb
coiacob <coiacob@pentalog.com>
2023-08-14 18:05:55 +0300

    initial commit

--- src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -3,6 +3,11 @@ package com.easybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,29 +17,53 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /**
-         * Custom security configuration
-         */
         http.authorizeHttpRequests(requests -> requests
                         .requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()
                         .requestMatchers("/notices", "/contact").permitAll())
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
-
-        /**
-         * Configuration to deny all the requests
-         */
-//        http.authorizeHttpRequests(requests -> requests
-//                        .anyRequest().denyAll())
-//                .formLogin(withDefaults())
-//                .httpBasic(withDefaults());
-        /**
-         * Configuration to permit all the requests
-         */
-//        http.authorizeHttpRequests(requests -> requests
-//                        .anyRequest().permitAll())
-//                .formLogin(withDefaults())
-//                .httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        /**
+         * Approach 1 where we use withDefaultPasswordEncoder() method
+         * while creating the user details
+         */
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+        /**
+         * Approach 2 where we use NoOpPasswordEncoder Bean
+         * while creating the user details
+         */
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
